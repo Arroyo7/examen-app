5083b73250c6e7a01cfb6c10c36afc0ab25cce95
Barcelona1996 <aarroyoescri@gmail.com>
2024-06-11 08:04:46 +0200

    segundo commit

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+export class FormComponent {
+
+  mForm = this.fb.group({
+
+      name: ["Madrid"]
+  })
+  constructor(private fb: FormBuilder){
+
+  }
+}
